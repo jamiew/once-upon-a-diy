1f43791ba134f0d3ad868e49f0c6da63cc5774f6
Jamie Wilkinson <jamie@jamiedubs.com>
2019-03-11 20:10:13 -0400

    Use postgres instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_11_232738) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stories", force: :cascade do |t|
     t.string "author"
     t.string "title"
